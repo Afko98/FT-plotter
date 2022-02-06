540058cb04fe4ea32ff17c0c2b6d8468ab6d0904
Afko98 <miki.velic@hotmail.com>
2022-02-06 23:22:07 +0100

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Location = new System.Drawing.Point(12, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 400);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.Location = new System.Drawing.Point(473, 38);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(450, 400);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(394, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "transform";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(550, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "abs(F(jw))";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(632, 11);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Re(F(jw))";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(713, 11);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Im(F(jw))";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(69, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(935, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Graphics g2;
+        bool mouse_down = false;
+        int[] time_function = new int[450];
+        double[] abs_function = new double[450];
+        double[] f_re = new double[450];
+        double[] f_im = new double[450];
+        Pen pen,pen_red;
+        bool abs = true, re = false, im = false;
+        int lastIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+            g2 = panel2.CreateGraphics();
+            g2.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            pen = new Pen(Color.Black, 1);
+            pen_red = new Pen(Color.Red, 1);
+        }
+     
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            g.DrawLine(pen_red, new Point(0, 200), new Point(450, 200));
+            g.DrawLine(pen_red, new Point(225, 0), new Point(225, 400));
+            for (int i=0; i < 450; i++)
+            {
+                time_function[i] = 200;
+            }
+        }
+        private void addGrid()
+        {
+            
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            g2.DrawLine(pen_red, new Point(0, 200), new Point(450, 200));
+            g2.DrawLine(pen_red, new Point(225, 0), new Point(225, 400));
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse_down = true;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            if (mouse_down)
+            {
+                time_function[e.X] =e.Y;
+                g.DrawLine(pen, new Point(e.X, time_function[e.X]), new Point(e.X - 1, time_function[e.X - 1]));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            abs = true;
+            im = false;
+            re = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            re = true;
+            abs = false;
+            im = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            im = true;
+            re = false;
+            abs = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 450; i++)
+                time_function[i] = (int)(200+100*Math.Sin(0.05f * (i - 225)));
+            mouse_down = false;
+            g.Clear(Color.White);
+            g.DrawLine(pen_red, new Point(0, 200), new Point(450, 200));
+            g.DrawLine(pen_red, new Point(225, 0), new Point(225, 400));
+            for (int i = 0; i < 450; i++)
+            {
+                if (i > 0)
+                    g.DrawLine(pen, new Point(i, time_function[i]), new Point(i - 1, time_function[i]));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            g.DrawLine(pen_red, new Point(0, 200), new Point(450, 200));
+            g.DrawLine(pen_red, new Point(225, 0), new Point(225, 400));
+            for (int i = 0; i < 450; i++)
+            {
+                time_function[i] = 200;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            f_re = new double[450];
+            f_im = new double[450];
+            for (int i = 0; i < 450; i++)
+            {
+                for (int j = 0; j < 450; j++)
+                {
+                    f_re[i] = f_re[i] + ((double)(200 - time_function[j]) * Math.Cos((double)(i - 225) * j*0.0005f));
+                    f_im[i] = f_im[i] - ((double)(200 - time_function[j]) * Math.Sin((double)(i - 225) * j*0.0005f));
+                }
+                abs_function[i] = Math.Sqrt(Math.Pow(f_re[i], 2) + Math.Pow(f_im[i], 2))/200f;
+            }
+            g2.Clear(Color.White);
+            g2.DrawLine(pen_red, new Point(0, 200), new Point(450, 200));
+            g2.DrawLine(pen_red, new Point(225, 0), new Point(225, 400));
+            if (abs)
+                for (int i = 0; i < 450; i++)
+                {
+                    if (i > 0)
+                        g2.DrawLine(pen, new Point(i, (int)(200-abs_function[i])), new Point(i - 1, (int)(200-abs_function[i])));
+                }
+            if(re)
+                for (int i = 0; i < 450; i++)
+                {
+                    if (i > 0)
+                        g2.DrawLine(pen, new Point(i, (int)(200 - (f_re[i] / 200f))), new Point(i - 1, (int)(200 - (f_re[i] / 200f))));
+                }
+            if(im)
+                for (int i = 0; i < 450; i++)
+                {
+                    if (i > 0)
+                        g2.DrawLine(pen, new Point(i, (int)(200 - (f_im[i] / 200f))), new Point(i - 1, (int)(200 - (f_im[i]/200f))));
+                }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse_down = false;
+            g.Clear(Color.White);
+            g.DrawLine(pen_red, new Point(0, 200), new Point(450, 200));
+            g.DrawLine(pen_red, new Point(225, 0), new Point(225, 400));
+            for (int i = 0; i < 450; i++)
+            {
+                if (i > 0)
+                    g.DrawLine(pen, new Point(i, time_function[i]), new Point(i - 1, time_function[i]));
+            }
+        }
+    }
+}
